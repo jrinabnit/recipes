c807cb7a3efc7f35c6373e0147373c9d4aee6cd9
jrinabnit <jrinabnit@gmail.com>
2021-05-04 12:46:01 +0800

    updating edit functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -14,6 +15,7 @@ import { DropdownDirective } from './shared/dropdown.directive';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
+import { RecipeService } from './recipes/recipe.service';
 
 @NgModule({
 	declarations: [
@@ -29,8 +31,8 @@ import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component
 		RecipeStartComponent,
 		RecipeEditComponent,
 	],
-	imports: [ BrowserModule, AppRoutingModule ],
-	providers: [ ShoppingListService ],
+	imports: [ BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule ],
+	providers: [ ShoppingListService, RecipeService ],
 	bootstrap: [ AppComponent ],
 })
 export class AppModule {}
